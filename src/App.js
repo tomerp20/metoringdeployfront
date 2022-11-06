fd6dbd28c97cb4d981a620d61151fc5027b3418d
= <tomerp20@users.noreply.github.com>
2022-11-06 11:52:29 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import { useEffect, useState } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const [tweets, setTweets] = useState([]);
+  useEffect(() => {
+    const getInfo = async () => {
+      const response = await fetch('https://mentoringclasstomer.herokuapp.com/tweets/');
+      const data = await response.json();
+      setTweets(data.data)
+    }
+    getInfo()
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello
+      {tweets.length > 0 && 
+      tweets.map(tweet => (<div>{JSON.stringify(tweet)}</div>))
+        
+      }
     </div>
   );
 }
